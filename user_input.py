43989c379822a3a2459ca2b4858a7e3576240e4e
Gibril Sonko <70228287+Gibrilsonko30@users.noreply.github.com>
2024-02-23 04:42:07 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old you? "))
+location = input("What is your location? ")
+print(f'Hello {name}, you are {age} and your location is {location}')
+
